0923b7626faeceeec4ef81d6f65ad05593941bad
Livier Stephanie <stephanielivier@gmail.com>
2025-03-30 22:14:27 +1300

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,36 @@
+pets = {
+    'dogs': [
+        {
+            'name': 'Spot',
+            'age': 2,
+            'breed': 'Dalmatian',
+            'description': 'Spot is an energetic puppy who seeks fun and adventure!',
+            'url': 'https://www.purina.es/sites/default/files/styles/ttt_image_original/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-07Dalmatian.webp?itok=BGft0XDv'
+        },
+        {
+            'name': 'Shadow',
+            'age': 4,
+            'breed': 'Border Collie',
+            'description': 'Eager and curious, Shadow enjoys company and can always be found tagging along at your heels!',
+            'url': 'https://dogtime.com/wp-content/uploads/sites/12/2023/09/GettyImages-164855910-1-e1694532950609.jpg?w=1024'
+        }
+    ],
+    'cats': [
+        {
+            'name': 'Snowflake',
+            'age': 1,
+            'breed': 'Tabby',
+            'description': 'Snowflake is a playful kitten who loves roaming the house and exploring.',
+            'url': 'https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg'
+        }
+    ],
+    'rabbits': [
+        {
+            'name': 'Easter',
+            'age': 4,
+            'breed': 'Mini Rex',
+            'description': 'Easter is a sweet, gentle rabbit who likes spending most of the day sleeping.',
+            'url': 'https://www.humaneworld.org/sites/default/files/styles/responsive_1_87_1_2900w/public/2022-09/wild-rabbit-166068.jpg.webp?itok=YAMI42x3'
+        }
+    ]
+}
